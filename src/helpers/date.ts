1a23a8f552a65019ba79bf40c759dfea03e96718
Ben Girardet <ben@platform5.ch>
2021-05-06 10:01:01 +0200

    improve date moment helper and recurrence

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,10 +1,30 @@
 import moment from 'moment';
 
 export class DateHelper {
-  public static moment(date: string | moment.Moment): undefined | moment.Moment {
+  public static moment(date: string | Date | moment.Moment, suggestedFormat?: string | string[]): undefined | moment.Moment {
     if (!date) return undefined;
     let m: moment.Moment;
     if (typeof date === 'string') {
+
+      const seemsIsoString = date.includes('T') && date.includes('Z');
+      if (seemsIsoString) {
+        m = moment(date, 'YYYY-MM-DDTHH:mm:ss.SSSSZ');
+        if (m.isValid()) {
+          return m;
+        }
+      }
+
+      if (suggestedFormat) {
+        const formats = Array.isArray(suggestedFormat) ? suggestedFormat : [suggestedFormat];
+        for (const format of formats) {
+          m = moment(date, format);
+          if (m.isValid()) {
+            return m;
+          }
+          m = undefined;
+        }
+      }
+
       if (date.length === 10 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-') {
         m = moment(date, 'DD-MM-YYYY');
       } else if (date.length === 16 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-' && date.substr(10, 1) === ' ' && date.substr(13, 1) === ':') {
@@ -28,6 +48,8 @@ export class DateHelper {
       } else {
         m = moment(date);
       }
+    } else if (date instanceof Date) {
+      m = moment(date);
     } else if (!moment.isMoment(date)) {
       m = moment(date);
       if (!m.isValid) {
@@ -38,5 +60,54 @@ export class DateHelper {
     }
 
     return m;
+  }
+
+  public static recurrence(
+    start: moment.Moment, 
+    end: moment.Moment, 
+    unit: 'w' | 'm', 
+    frequency: '1/1' | '1/2' | '1/3', 
+    daysOfWeekOrMonth: number[], 
+    options?: {nbDaysLimit?: number}): moment.Moment[] {
+
+    if (!start.isValid() || !end.isValid || end.isBefore(start)) {
+      throw new Error('Invalid start/end dates, end must be after start');
+    }
+
+    const nbDaysLimit = options?.nbDaysLimit || 365 *5;
+    if (end.diff(start, 'days') > nbDaysLimit) {
+      throw new Error('Invalid start/end dates, end is too far from start');
+    }
+
+    const current = start.clone();
+    const dates: moment.Moment[] = [];
+    const modulos = {
+      '1/1': 1,
+      '1/2': 2,
+      '1/3': 3,
+      '1/4': 4
+    };
+    let unitNb = 1;
+    let unitIndex = unit === 'w' ? current.week() : current.month();
+    while(current.isBefore(end)) {
+      if(unit === 'w' && !daysOfWeekOrMonth.includes(current.isoWeekday())) {
+        current.add(1, 'day');
+        continue; // ignored as not the right day of week
+      }
+      if(unit === 'm' && !daysOfWeekOrMonth.includes(current.day())) {
+        current.add(1, 'day');
+        continue; // ignored as not the right day of week
+      }
+      const currentUnitIndex = unit === 'w' ? current.week() : current.month();
+      if (unitIndex !== currentUnitIndex) {
+        unitNb++;
+      }
+      unitIndex = currentUnitIndex;
+      if (unitNb % modulos[frequency] === 0) {
+        dates.push(current.clone());
+      }
+      current.add(1, 'day');
+    }
+    return dates;
   }
 }
