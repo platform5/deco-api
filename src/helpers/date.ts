1e0ce6f9776d1421d8a2dc952966a5c0449fa3b0
Ben Girardet <ben@platform5.ch>
2021-07-05 10:17:27 +0200

    Fix frequency typing

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -66,7 +66,7 @@ export class DateHelper {
     start: moment.Moment, 
     end: moment.Moment, 
     unit: 'w' | 'm', 
-    frequency: '1/1' | '1/2' | '1/3', 
+    frequency: '1/1' | '1/2' | '1/3' | '1/4' | '1/5' | '1/6' | '1/7' | '1/8' | '1/9' | '1/10', 
     daysOfWeekOrMonth: number[], 
     options?: {nbDaysLimit?: number}): moment.Moment[] {
 
