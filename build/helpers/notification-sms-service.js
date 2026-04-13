89f75332f34e24375fa264030664904e99cbc290
Tamu <5020311+Tamu@users.noreply.github.com>
2026-04-13 22:56:06 +0200

    Build

--- build/helpers/notification-sms-service.js
+++ build/helpers/notification-sms-service.js
@@ -23,12 +23,13 @@ class NotificationSMSService {
         this.templatePath = templatePath;
     }
     send(mobile, template, data, templateSettings) {
+        var _a, _b, _c, _d;
         return __awaiter(this, void 0, void 0, function* () {
             data.cache = true;
             data.pretty = false;
             let from = 'Info';
             if (data.app && data.app.smtpConfigFromName) {
-                from = data.app.smtpConfigFromName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(" ", "%20");
+                from = data.app.smtpConfigFromName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g, "");
             }
             let options = Object.assign({}, data, { pretty: false, cache: true });
             const templatePath = (templateSettings === null || templateSettings === void 0 ? void 0 : templateSettings.rootPath) ? templateSettings.rootPath : this.templatePath;
@@ -53,35 +54,47 @@ class NotificationSMSService {
                 : pug_1.default.renderFile(templatePath + '/' + template + '/sms.pug', options);
             const apiToken = this.accessToken;
             const smsapi = new SMSAPI(apiToken);
-            let endpoint = '/sms.do?from=' + from;
-            smsapi.sms.endpoint = endpoint;
+            const targetMobile = mobile.startsWith('+') ? mobile.substring(1) : mobile;
             try {
-                const result = yield smsapi.sms.sendSms(mobile, txt);
-                console.log(result);
-                let allSended = false;
+                let result = yield smsapi.sms.sendSms(targetMobile, txt, { from });
                 if (result.list && result.list.length > 0) {
-                    console.log(result);
-                    for (const item of result.list) {
-                        try {
-                            const check = yield smsapi.hlr.check(item.submittedNumber);
-                            if (check.status == 'OK') {
-                                allSended = true;
-                            }
-                            else {
-                                allSended = false;
-                                break;
-                            }
-                        }
-                        catch (err) {
-                            console.log(err);
-                        }
-                    }
-                    return allSended;
+                    return true;
                 }
-                return true;
+                return false;
             }
             catch (err) {
-                console.log(err.data.message);
+                const errorMessage = ((_a = err) === null || _a === void 0 ? void 0 : _a.message) || '';
+                // Handle SSL certificate error by temporarily bypassing validation (fail-safe)
+                if (errorMessage.includes('self signed certificate')) {
+                    const originalRejectValue = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
+                    try {
+                        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+                        // Retry with current settings but bypassing TLS validation
+                        const retryResult = yield smsapi.sms.sendSms(targetMobile, txt, { from });
+                        if (retryResult.list && retryResult.list.length > 0) {
+                            return true;
+                        }
+                    }
+                    catch (retryErr) {
+                        // Ignore additional errors
+                    }
+                    finally {
+                        process.env.NODE_TLS_REJECT_UNAUTHORIZED = originalRejectValue;
+                    }
+                }
+                // Retry with default sender 'Info' if custom 'from' might be the issue
+                if (from !== 'Info') {
+                    try {
+                        const retryResult = yield smsapi.sms.sendSms(targetMobile, txt, { from: 'Info' });
+                        if (retryResult.list && retryResult.list.length > 0) {
+                            return true;
+                        }
+                    }
+                    catch (retryErr) {
+                        // Ignore additional errors
+                    }
+                }
+                console.log(((_c = (_b = err) === null || _b === void 0 ? void 0 : _b.data) === null || _c === void 0 ? void 0 : _c.message) || ((_d = err) === null || _d === void 0 ? void 0 : _d.message) || err);
                 return false;
             }
         });
